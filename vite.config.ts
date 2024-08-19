a7a6f46c4247ff1abb189c81ec5e64d3bc101205
Dawid-Femin <d.femin@wp.pl>
2024-08-19 23:57:03 +0200

    Improved export config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,8 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import { resolve } from 'path';
-//
-// export default defineConfig({
-//   build: {
-//     lib: {
-//       entry: resolve(__dirname, 'src/index.ts'),
-//       name: 'dawidfemin-components2024',
-//       formats: ['es', 'cjs'],
-//       fileName: (format) => `dawidfemin-components2024.${format}.js`,
-//     },
-//     rollupOptions: {
-//       external: ['react', 'react-dom'],
-//       output: {
-//         globals: {
-//           react: 'React',
-//           'react-dom': 'ReactDOM',
-//         },
-//       },
-//     },
-//   },
-//   plugins: [react()],
-// });
-
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -38,8 +12,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "./src/index.ts"),
-      name: "SimpleUI",
-      fileName: "simple-ui",
+      name: "index",
+      fileName: "index",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
